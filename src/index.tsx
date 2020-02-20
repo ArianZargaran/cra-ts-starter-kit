75db31f3f0bb761ee92eaa4970108cd60bdb5865
Arian Zargaran <arian.zargaran@cabify.com>
2020-02-21 00:32:23 +0100

    React-redux and redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from "react-redux";
+import { store } from "state/store";
 
 import './styles/reset.css';
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// bootstrapActions(store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
