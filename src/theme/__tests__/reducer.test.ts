7d2a78a36ce2d3ee0f040b429641ae2cf895008d
Arian Zargaran <arian.zargaran@cabify.com>
2020-03-01 11:34:20 +0100

    Jest and Enzyme config

---
+++ src/theme/__tests__/reducer.test.ts
@@ -0,0 +1,17 @@
+// import { themeInitialState } from "./initial-state";
+// import { ThemeEnum } from "theme/ThemeEnum";
+// import { THEME_ACTIONS_TYPES, ThemeActionTypes } from "./action-creators";
+
+// export const themeReducer: React.Reducer<ThemeEnum, ThemeActionTypes> = (
+//   theme = themeInitialState,
+//   action
+// ) => {
+//   switch (action.type) {
+//     case THEME_ACTIONS_TYPES.TOGGLE_THEME:
+//       return theme === ThemeEnum.light ? ThemeEnum.dark : ThemeEnum.light;
+//     case THEME_ACTIONS_TYPES.SET_DARK_THEME:
+//       return ThemeEnum.dark;
+//     default:
+//       return theme;
+//   }
+// };
