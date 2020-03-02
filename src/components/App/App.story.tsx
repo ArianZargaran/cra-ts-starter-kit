2c9477d06cb620a2323cd657c5ff33482f39ed76
Arian Zargaran <arian.zargaran@cabify.com>
2020-03-02 20:47:05 +0100

    Knob Addons

--- src/components/App/App.story.tsx
+++ src/components/App/App.story.tsx
@@ -2,23 +2,24 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { App } from "components/App/App";
 import { ThemeProvider } from "theme/state/provider";
+import { text } from "@storybook/addon-knobs";
 
-const AppComponent = () => {
+const AppComponent = ({ copy }: { copy: string }) => {
   return (
     <ThemeProvider>
-      <App />
+      <App copy={copy} />
     </ThemeProvider>
-  )
-}
+  );
+};
 
 storiesOf("App/Master", module)
   .add("On Light Mode", () => (
     <div className="light-mode-root">
-      {AppComponent()}
+      {AppComponent({ copy: text("copy", "abcde") })}
     </div>
   ))
   .add("On Dark Mode", () => (
     <div className="dark-mode-root">
-      {AppComponent()}
+      {AppComponent({ copy: text("copy", "12345") })}
     </div>
   ));
